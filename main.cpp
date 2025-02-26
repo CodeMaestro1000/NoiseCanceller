e45d86faa71a7cf7e60714f7b269c320feb95d2c
timothyakoji <timothyakoji@gmail.com>
2025-02-27 00:20:22 +0100

    Use signals SIGINT and SIGTERM to terminate operation

--- main.cpp
+++ main.cpp
@@ -1,11 +1,23 @@
+#include <csignal>
+#include <iostream>
 #include "AudioRouter.h"
 
-int main() {
+volatile sig_atomic_t stop; // don't optimize!
 
+void interruptHandler(int signum) {
+    stop = 1;
+}
+
+int main() {
+    signal(SIGINT, interruptHandler); // Capture CTRL + C to clean up before exiting.
+    signal(SIGTERM, interruptHandler); // Capture button event in CLION
     PortAudioRouter paRouter;
 
+    paRouter.selectInputDevice(1); // macbook default
+    paRouter.selectOutputDevice(0); // blackhole
+
     paRouter.startCapture();
-    Pa_Sleep(60 * 1000);
+    while (!stop){}
     paRouter.stopCapture();
 
     return 0;
